2af32fafd34ce37aa1134ff61446574a27c4946e
FranciscoGSilverio <franciscogsilverio@gmail.com>
2026-02-19 22:39:42 -0300

    Add go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module webserverrc
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+	fmt.Println("Starting server on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
