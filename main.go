605bed15812705b5879e565fe4a3edddb9768add
FranciscoGSilverio <franciscogsilverio@gmail.com>
2026-02-22 19:04:33 -0300

    Testing argo cd syncronization

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		w.Write([]byte("Hello, World! I was deployed by Argo CD"))
 	})
 	fmt.Println("Starting server on port 8080")
 	http.ListenAndServe(":8080", nil)
